131f5577db1391bd2a80987d55c1a09f1ad78119
SidSpears <sidspears55@gmail.com>
2020-07-12 13:33:08 +0300

    testing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".menu__link").each(function(){
+        $(this).click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"}, 1000);
+            return false;
+    })
+    });
+});
